e224bb31dbff1cb80ea2a25ac50a9e7804a35ba4
Matthias Matousek <m@tou.io>
2011-11-16 17:05:25 +0100

    the ircbot can now connect to a server and join a room

--- ircbot.py
+++ ircbot.py
@@ -12,29 +12,73 @@
 # ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-import socket, logging, sys
+import socket, logging, sys, ircbotconf
 from threading import Thread
 
 class IrcBot(Thread):
 
-    def __init__(self, server, nick, port=6667):
+    def __init__(self, server, nick, user, room, port=6667):
+        """Set up the IrcBot by setting necessary fields. """
+
+        Thread.__init__(self)
+
         # set up the data
         self.server = server
         self.port = port
         self.nick = nick
+        self.user = user
+        self.room = room
 
         # we need a socket to connect to the irc server
         self.sock = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
 
     def run(self):
+        """Starts the bot by connecting to the server and joining a room."""
+
         try:
             logging.info('trying to connect to %s:%d' % (self.server, self.port))
-            self.sock
+            self.sock.connect( (self.server, self.port) )
+            connected = True
             logging.info('connected to %s:%d' % (self.server, self.port))
         except:
             logging.fatal('unable to connect to %s:%d' % (self.server, self.port))
             sys.exit(1)
 
+        logging.debug('trying to register nick "%s"' %self.nick)
+        self.send('NICK %s' % self.nick)
+
+        logging.debug('trying to register user "%s"' %self.user)
+        self.send('USER %s' %self.user)
+
+        logging.debug('trying to join room "%s"' %self.room)
+        self.send('JOIN %s' % self.room)
+
+        # infinite loop handling data
+        while True:
+            data = self.sock.recv(4096)
+
+            logging.debug('received %s' % data)
+
+            # stop if empty data is received
+            if data == '': 
+                logging.info('disconnected')
+                break
+            
+            self.handle(data)
+            
+    def handle(self, data):
+        """Handles received data"""
+        pass
 
 
+    def send(self, data):
+        """Sends the given data to the IRC server."""
+        self.sock.send( data + '\r\n' )
+        logging.debug('sent %s' % data)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    bot = IrcBot(ircbotconf.server, ircbotconf.nick, ircbotconf.user, ircbotconf.room)
+    bot.start()
 
