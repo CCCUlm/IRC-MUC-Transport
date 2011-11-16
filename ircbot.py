7c5110ed6fc736a3ed3663b444acafdccc6f567d
Matthias Matousek <m@tou.io>
2011-11-16 16:04:17 +0100

    the bot got a socket now and tries to connect to a given server

--- ircbot.py
+++ ircbot.py
@@ -12,4 +12,29 @@
 # ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
+import socket, logging, sys
+from threading import Thread
+
+class IrcBot(Thread):
+
+    def __init__(self, server, nick, port=6667):
+        # set up the data
+        self.server = server
+        self.port = port
+        self.nick = nick
+
+        # we need a socket to connect to the irc server
+        self.sock = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
+
+    def run(self):
+        try:
+            logging.info('trying to connect to %s:%d' % (self.server, self.port))
+            self.sock
+            logging.info('connected to %s:%d' % (self.server, self.port))
+        except:
+            logging.fatal('unable to connect to %s:%d' % (self.server, self.port))
+            sys.exit(1)
+
+
+
 
